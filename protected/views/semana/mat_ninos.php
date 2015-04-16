9ffb434420d3b1bea7c78543ac3b71c666615419
calonso-conabio <calonso@conabio.gob.mx>
2015-04-15 19:12:16 -0500

    cambios en materiales

--- protected/views/semana/mat_ninos.php
+++ protected/views/semana/mat_ninos.php
@@ -1,11 +1,39 @@
 <br>
 <p style="color:#C60">Materiales para niños</p>
-                            
+
 <!-- Two -->
-<section id="two">
-		<article>
-		<p><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/titNinos.png" width="188" height="114"></p>
-				<p><a href="http://www.geologia.unam.mx/igl/index.php/difusion-y-divulgacion/312-terramovil/782-terramovil" target="_blank">Terramovil</a>. Proyecto de divulgaci&oacute;n de Ciencias de la Tierra para alumnos de primaria y secundaria enfocado en el entorno geol&oacute;gico, hidrol&oacute;gico y de los suelos de la Cuenca de M&eacute;xico.</p>
-				<p>&nbsp;</p>
-				</article>
-				</section>
+					<section id="two">
+                    	<article>
+                    	  <table width="90%" border="0">
+                    	    <tr>
+                    	      <td width="34%" bgcolor="#E8E4D0"><p><span style="color:#C60"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icweb_07.png" width="25" height="22"> Web</span></p>
+                    	        <p><a href="http://www.biodiversidad.gob.mx/ninos/ninos.html" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/logoniños.png" width="200" height="126"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                                </a><span class="piesImg">Comisi&oacute;n Nacional para el Conocimiento y Uso de la Biodiversidad (CONABIO)</span></p>
+                    	        <p><a href="http://www.geologia.unam.mx/igl/index.php/difusion-y-divulgacion/312-terramovil/782-terramovil" target="_blank"><br>
+                   	            <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/webTerraMovil.jpg" width="200" height="111"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                  	            </a><span class="piesImg"><strong>Terram&oacute;vil<br />
+                  	            </strong>Instituto de Geolog&iacute;a, UNAM</span></p>
+                    	        <p><a href="http://cuentame.inegi.org.mx/territorio/suelo.aspx?tema=T" target="_blank"><br>
+                   	            <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/webInegi.jpg" width="200" height="111"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                                </a><span class="piesImg"><strong>Suelos<br />
+                                </strong>Instituto Nacional de Estad&iacute;stica y Geograf&iacute;a (INEGI)</span></p>
+                    	        <p><a href="http://www.educarchile.cl/ech/pro/app/detalle?ID=212994" target="_blank"><br>
+                   	            <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/web_educarChile.jpg" width="200" height="148"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                                </a><span class="piesImg"><strong>Formaci&oacute;n de suelos<br />
+                                </strong>Educar Chile</span></p>
+                    	        <p><a href="http://www.educarchile.cl/ech/pro/app/detalle?id=217007" target="_blank"><br>
+                   	            <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/web_educarChile_1.jpg" width="200" height="148"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                                </a><span class="piesImg"><strong>Impactos de la actividad humana sobre los suelos<br />
+                                </strong>Educar Chile</span></p></td>
+                    	      <td width="35%" valign="top" bgcolor="#E8E4D0"><p><span style="color:#C60"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icDoc_07.png" width="25" height="22"> Documentos</span></p>
+                                <p><a href="http://www.fao.org/fileadmin/user_upload/red-icean/docs/Crear_y_manejar_un_huerto_escolar_-_FAO.pdf" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/imgHuertosEscFAO.png" width="200" height="127"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                              </a><span class="piesImg"><strong>Crear y manejar un huerto escolar<br>
+                              </strong>Organizaci&oacute;n de las Naciones Unidas para la Agriculutra y la Alimentaci&oacute;n (FAO)</span></p>
+                              <p><a href="http://www.fao.org/docrep/field/009/as239s/as239s.pdf" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/imgEducaHuerto_FAO.png" width="200" height="127"></a><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><br>
+                              </a><span class="piesImg"><strong>Educando con el huerto escoalr<br>
+                              </strong>Organizaci&oacute;n de las Naciones Unidas para la Agriculutra y la Alimentaci&oacute;n (FAO)</span>                              </p></td>
+                    	      <td width="31%" bgcolor="#E8E4D0"><p>&nbsp;</p></td>
+                  	      </tr>
+                  	    </table>
+                   	  </article>
+					</section>
--- protected/views/semana/materiales.php
+++ protected/views/semana/materiales.php
@@ -2,38 +2,56 @@
 <p style="color:#C60">Materiales</p>
 
 <!-- Two -->
-					<section id="two">
-					  <article><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/titVideos.png" width="138" height="114"></article>
+<section id="two">
 					  <div class="row">
-                      	  <article class="6u 12u$(xsmall) work-item">
-						   <iframe class="image fit thumb" width="350" height="197" src="https://www.youtube.com/embed/N2nuRrIIams?rel=0" frameborder="0" allowfullscreen></iframe>
-							<p><strong>La naturaleza nos habla</strong> | Conservaci&oacute;n Internacional </p>
-</article>
-							<article class="6u$ 12u$(xsmall) work-item">
-								<iframe class="image fit thumb" width="350" height="197" src="https://www.youtube.com/embed/gsrULE0v2Tc?rel=0" frameborder="0" allowfullscreen></iframe>
-							  <p><strong>Inc&oacute;gnitos</strong> | CONABIO</p>
-						</article>
-							
-                            <article class="6u 12u$(xsmall) work-item">
-								<iframe class="image fit thumb" width="350" height="197" src="https://www.youtube.com/embed/1DEsfW7kJi4?rel=0" frameborder="0" allowfullscreen></iframe>
-                                <p><strong>El suelo: un recurso oculto</strong> | Organizaci&oacute;n de las Naciones Unidas para la Alimentaci&oacute;n y la Agricultura (FAO)</p>
-						</article>
-							
-                        <article class="6u 12u$(xsmall) work-item">
-								<iframe class="image fit thumb" width="263" height="197" src="https://www.youtube.com/embed/DSt1h8HZV0E?rel=0" frameborder="0" allowfullscreen></iframe>
-                                <p><strong>Indígenas recuperan suelos erosionados San Cristobal Oaxaca, México</strong> | CONAFOR</p>
-							</article>
-                            
-                            <article class="6u 12u$(xsmall) work-item">
-								<iframe  class="image fit thumb"  width="263" height="197" src="https://www.youtube.com/embed/H5U6Agutf_Q?rel=0" frameborder="0" allowfullscreen></iframe>
-                                <p><strong>Nueve mil años de agriculturta en México</strong> | Canal GeaVideo</p>
-							</article>
-                            
-                             <article class="6u 12u$(xsmall) work-item">
-								<iframe class="image fit thumb" width="350" height="197" src="https://www.youtube.com/embed/qa309qhleis?list=PL0FC72989A7B47BFC" frameborder="0" allowfullscreen></iframe>
-                                <p><strong>Historia de la agricultura en M&eacute;xico</strong> | Cl&iacute;o</p>
-							</article>
-					  
-                      
-                      </div>
+                   	    <article>
+                   	      <table width="90%" border="0">
+                      	      <tr>
+                      	        <td width="34%" bgcolor="#E8E4D0"><p><span style="color:#C60"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icvideo_07.png" width="25" height="22" /> Videos</span></p>
+                      	          <p>                      	          <a href="https://www.youtube.com/watch?v=N2nuRrIIams" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_ConservaInt.jpg" width="200" height="126"><br>
+                      	          </a><span class="piesImg"><strong>La naturaleza nos habla<br>
+                      	          </strong>Conservaci&oacute;n Internacional</span></p>
+                      	          <p><a href="https://www.youtube.com/watch?v=gsrULE0v2Tc" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_Incognitos.jpg" width="200" height="126"><br>
+                   	              </a><span class="piesImg"><strong>Inc&oacute;gnitos | 
+                    	              </strong>CONABIO               	                  </span></p>
+                      	          <p><a href="https://www.youtube.com/watch?v=1DEsfW7kJi4" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_FAO.jpg" width="200" height="126"></a><br>
+               	                  <span class="piesImg"><strong>El suelo: un recurso oculto |           	                    </strong>FAO</span></p>
+                      	          <p><a href="https://www.youtube.com/watch?v=DSt1h8HZV0E" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_IAgropecuaria.jpg" width="200" height="126"><br>
+                      	          </a><span class="piesImg"><strong>Recuperan suelos erosionados en Oaxaca<br>
+           	                      </strong>Imagen Agropecuaria</span></p>
+                      	          <p><a href="https://www.youtube.com/watch?v=H5U6Agutf_Q" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_Geavideo.jpg" width="200" height="126"><br>
+                      	            </a><span class="piesImg"><strong>Nueve mil a&ntilde;os de agricultura en M&eacute;xico<br>
+       	                          </strong>Canal GeaVideo</span></p>
+                      	          <p><a href="https://www.youtube.com/watch?list=PL0FC72989A7B47BFC&v=qa309qhleis" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_clio.jpg" width="200" height="126"><br>
+                      	            </a><span class="piesImg"><strong>Historia de la agricultura en M&eacute;xico<br>
+                      	            </strong>Cl&iacute;o</span></p>
+                      	          <p><a href="https://vimeo.com/116705021" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/video_MRVMx.jpg" width="200" height="126"><br>
+                      	            </a><span class="piesImg"><strong>Papel del suelo en el ciclo del carbono<br>
+   	                              </strong>CONAFOR</span></p></td>
+                      	        <td width="35%" valign="top" bgcolor="#E8E4D0"><p><span style="color:#C60">Logotipo</span></p>
+                      	          <p><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/logo_sdb5_baja.png" width="200" height="100"><br>
+                   	              <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icdescarga_03.png" width="30" height="23"> <span class="piesImg">Archivo: <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/logo_sdb5_alta.jpg" target="_blank">JPG</a> | <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/logo_sdb5_alta.png" target="_blank">PNG</a></span></p>
+                      	          <p><span style="color:#C60"><br>
+                   	              Promocional<br>
+                   	              </span> <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/imagen5semana.jpg" width="200" height="160"><br>
+                   	              <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icdescarga_03.png" width="30" height="23"> <span class="piesImg">Archivo: <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/promo5semana_carta.jpg" target="_blank">21 x 27 cm</a></span></p>
+                      	          <p><br>
+                      	            <span style="color:#C60"> Redes sociales<br>
+                   	              </span> <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/SDB5_facebook_peq.jpg" width="240" height="126"><br>
+                   	              <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icdescarga_03.png" width="30" height="23"> <span class="piesImg">Facebook: <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/SDB5_facebook.jpg" target="_blank">JPG</a></span></p>
+                      	          <p><span style="color:#C60"><br>
+                   	              Banners para sitios web<br>
+                   	              </span> <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/refBanner.png" width="200" height="117"><br>
+                   	              <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icdescarga_03.png" width="30" height="23"> <span class="piesImg"><a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/Banner5SDB_600.png" target="_blank">600 x 352 px </a> | <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/Banner5SDB_300.png" target="_blank">300 x 176 px</a> | <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/Banner5SDB_248.png" target="_blank">248 x 312 px</a></span></p></td>
+                      	        <td width="31%" bgcolor="#E8E4D0"><p><span style="color:#C60"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/icweb_07.png" width="25" height="22" /> Web</span></p>
+                      	          <p><a href="http://www.fao.org/soils-2015/soil-facts/es/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/IYS_logos_es.png" /></a><br />
+                                    <strong class="piesImg" style="color:#333">Organizaci&oacute;n de las Naciones Unidas para 
+                                  la Alimentaci&oacute;n y la Agricultura</strong></p>
+                                  <p>&nbsp;</p>
+                                  <p><a href="http://www.impactoambientalindividual.com/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/aplicacion/SDB_2015/Imagenes/WebCalculadora.png" width="200" height="127"><br>
+                                </a><strong class="piesImg" style="color:#333">Calculadora de huella ecol&oacute;gica para mexicanos</strong></p></td>
+                   	          </tr>
+                   	        </table>
+                   	    </article>
+					  </div>
 					</section>
