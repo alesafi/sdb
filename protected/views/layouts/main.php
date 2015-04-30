16eae701c468506a4411e4cfce1474418aebc5e3
calonso-conabio <calonso@conabio.gob.mx>
2015-04-30 17:13:56 -0500

    codigo de ganalytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -96,6 +96,20 @@ function MM_swapImage() { //v3.0
 			</div>
 
 		<!-- Footer -->
+		
+		        <!-- C\357\277\275digo para estad\357\277\275sticas en Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8226401-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- Fin del c\357\277\275digo para estad\357\277\275sticas en Google Analytics -->
+		
+		
 	</body>
 </html>                         
                      
