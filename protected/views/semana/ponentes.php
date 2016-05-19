21c4c3330875aba8885fcb97dc7b94f3bda52106
jaluifa <lfabiola@gmail.com>
2016-05-19 18:26:59 -0500

    Actualizacion en ponentes

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -27,18 +27,21 @@ $( "#accordion" ).accordion({
 <a href="http://maestriaeditorial.xoc.uam.mx/index.php?option=com_content&view=article&id=242:dgoldin&catid=39:coordinadores&Itemid=91" target="_blank">Ma. Daniel Goldin Halfon</a><br><br>
 
 <span class="tit_materiales">Fotografía de naturaleza</span><br>
+<a href="https://www.linkedin.com/in/ivana-fernandez-stohanzlova-351899106" target="_blank">Ivana Fernández Stohanzlova</a><br>
+<a href="http://www.sladedesign.com.au/lead/component/comprofiler/userprofile/cpigueron" target="_blank">Celia Pigueron Wirz</a><br>
 <a href="https://twitter.com/letatdeschoses" target="_blank">Lic. Claudia Muzzi</a><br>
 <a href="http://luisjaviersandoval.photoshelter.com/#!/about" target="_blank">Biól. Luis Javier Sandoval</a><br>
 <a href="http://www.fulvioeccardi.com/" target="_blank">Biól. Fulvio Eccardi</a><br>
 <a href="http://jaguarnegro.mx/web/?page_id=90#.Vw_w-JMrLUI" target="_blank">Biól. Jorge Douglas Brandon Pliego</a><br>
+<a href="https://mx.linkedin.com/in/julio-jes%C3%BAs-meneses-92435726" target="_blank">Julio Meneses</a><br>
 <a href="http://mixfm.mx/locutores/inaki-manero/" target="_blank">Iñaki Manero</a><br><br>
 
 
 <span class="tit_materiales">Agricultura y pesca</span><br>
+<a href="https://es.wikipedia.org/wiki/Guillermo_Haro_Bélchez" target="_blank">Lic. Guillermo Haro Bélchez</a><br>
 <a href="http://www.uccs.mx/expertos/agricultura_alimentacion/alejandro-espinosa-calderon.html" target="_blank">Dr. Alejandro Espinosa Calderón</a><br>
 <a href="https://www.youtube.com/watch?v=hHgkl80g57k" target="_blank">Dr. Pablo Arenas Fuentes</a><br>
 <a href="http://www.literatura.bellasartes.gob.mx/acervos/index.php/catalogo-biobibliografico/1424?showall=1" target="_blank">Victor Ronquillo</a><br>
-<a href="http://causanatura.org/acerca-de-causanatura/ernesto-herrera/" target="_blank">Dr. Ernesto Herrera Guerra</a><br>
 <a href="http://www.imer.mx/reactor/author/leonardoferrera/" target="_blank">Leonardo Ferrera</a><br>
 <!--<a href="https://es.wikipedia.org/wiki/Karla_Iberia_Sánchez" target="_blank">Karla Iberia Sánchez</a><br>-->
 <a href="https://www.youtube.com/watch?v=rlu1E7PgLzs" target="_blank">Dr. Teobaldo Eguiluz Piedra</a><br><br>
@@ -51,14 +54,21 @@ $( "#accordion" ).accordion({
 <a href="http://www.ceiba.org.mx/quienes-somos/miembros/name/alfredo-arellano-guillermo/" target="_blank">Biól. Alfredo Arellano Guillermo</a><br>
 <a href="http://www.conafor.gob.mx:8080/documentos/docs/7/2355CV%20Enrique%20Jardel.pdf" target="_blank">M. en C. Enrique Jardel Peláez</a><br>
 <a href="https://www.youtube.com/watch?v=v8vxiwA4Ec4" target="_blank">Jordi Arenas</a><br>
-<!--<a href="" target="_blank">Lic. Salvador Villalobos</a><br>
-<a href="" target="_blank">Francisco Alanis</a>--><br><br>
+<a href="https://www.gob.mx/conanp/estructuras/andrew-john-rhodes-espinoza" target="_blank">Biól. Andrew Rhodes</a><br>
+<a href="http://causanatura.org/acerca-de-causanatura/ernesto-herrera/" target="_blank">Dr. Ernesto Herrera Guerra</a><br>
+<a href=" https://mx.linkedin.com/in/ra%C3%BAl-cort%C3%A9s-fern%C3%A1ndez-581a1a3b" target="_blank">Lic. Raúl Cortés</a><br><br>
 
 
 <span class="tit_materiales">Naturalista</span><br>
 <a href="http://conabio.inaturalist.org/people/heliographyst" target="_blank">Biól. Roberto Arreola Alemón</a><br>
 <a href="http://conabio.inaturalist.org/people/elizatorres" target="_blank">Biól. Elizabeth Torres Bahena</a><br>
 <a href="https://www.linkedin.com/in/astrid-domínguez-guerrero-a9833498" target="_blank">D.G. Astrid Domínguez Guerrero</a><br><br>
+
+
+<a href="http://www.mexicopelagico.org/#creditos" target="_blank">Jerónimo Prieto</a><br>
+<a href="https://en.wikipedia.org/wiki/Jorge_Cervera_Hauser" target="_blank">Jorge Cervera Hauser<b</a><br>
+<a href="http://rcastrocr.wix.com/lacuchara#!bio" target="_blank">Ricardo Castro</a><br><br>
+
 
 <span class="tit_materiales">Grupo musical invitado</span><br>
 <a href="https://www.facebook.com/fechascasaverdecolectivo" target="_blank">Casa Verde Colectivo</a>
