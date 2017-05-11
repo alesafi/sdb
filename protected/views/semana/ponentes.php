bb831e905d14734b597fd10df65aa29408423faa
jaluifa <lfabiola@gmail.com>
2017-05-11 12:46:37 -0500

    Cambio en ponentes

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -19,24 +19,36 @@ $( "#accordion" ).accordion({
 <h1>Ponentes</h1><br>
 
 <div style="width: 100%; float:left;">
-<!--<span class="tit_materiales">Biodiversidad es bienestar</span><br>-->
+<span>Del 22 al 26 de mayo en la Biblioteca Vasconcelos, en la Ciudad de México tendremos
+conversatorios sobre Turismo Sostenible con:</span><br><br>
+
+
+<span class="tit_materiales">Moderadores</span><br>
+
 Alejandro García / <a href="http://oncetv-ipn.net/fciencia/" target="_blank">Reportero de Ciencia Canal Once</a><br>
+<a href="http://biografia.cine.hispavista.com/n90320-erik-estrada" target="_blank">Erick Estrada</a> / Actor y productor<br>
+<a href="https://twitter.com/ViajaBonito?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2Fwww.viajabonitomx.com%2F" target="_blank">Marco Daniel Guzmán</a> / <a href="http://www.viajabonitomx.com/
+" target="_blank">Viaja Bonito</a><br><br>
+
+
+<span class="tit_materiales">Ponentes</span><br>
+
 Lucía Ruiz Bustos / <a href="https://adventuremexico.travel/organizaciones/conanp/" target="_blank">Comisión Nacional de Áreas Protegidas (<span class="conabio">Conanp</span>)</a><br>
 Claudia Valeria Sánchez / <a href="http://www.rita.com.mx/" target="_blank">Red Indígena de Turismo de México</a><br>
 Julia Carabias Lilo / <a href="http://naturamexicana.org.mx/" target="_blank">Natura Mexicana</a><br>
 <a href="https://www.facebook.com/alonsopatadeperrovera/?pnref=story" target="_blank">Alonso Vera Cantú</a> / Pata de Perro<br>
-<a href="http://biografia.cine.hispavista.com/n90320-erik-estrada" target="_blank">Erick Estrada</a> / Actor y productor<br>
 <a href="http://ib.unam.mx/directorio/194" target="_blank">Luis Zambrano</a> / Instituto de Biología UNAM<br>
 <a href="http://www.sedema.cdmx.gob.mx/secretaria/estructura/22" target="_blank">Luis Fueyo</a> / Comisión de Recursos Naturales de la Ciudad de México (<span class="conabio">Corena</span>)<br>
 Rodolfo Salinas / <a href="http://natoure.org/" target="_blank">Natoure.org</a><br>
 <a href="http://www.radioformula.com.mx/perfil.asp" target="_blank">Arturo Hernández</a> / <a href="https://www.facebook.com/Supercivicosmx/?fref=ts
 " target="_blank">Supercívicos</a><br>
 Renata Cao De la Fuente / <a href="http://www.biodiversidad.gob.mx/corredor/corredor.html" target="_blank"><span class="conabio">Conabio</span></a><br>
-<a href="https://twitter.com/ViajaBonito?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2Fwww.viajabonitomx.com%2F" target="_blank">Marco Daniel Guzmán</a> / <a href="http://www.viajabonitomx.com/
-" target="_blank">Viaja Bonito</a><br>
 <a href="https://www.gob.mx/sectur/estructuras/mtra-maria-teresa-solis-trejo" target="_blank">Teresa Solís</a> / <span class="conabio">Sectur</span><br>
 Vicente Ferreyra / <a href="http://sustentur.com.mx/" target="_blank"><span class="conabio">Sustentur</span></a><br>
-<br>
+<br><br>
+
+
+
 </div>
 
 
