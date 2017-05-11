49381de107f03d4cd2a8b716de770870cda945a7
jaluifa <lfabiola@gmail.com>
2017-05-11 12:56:43 -0500

    Nuevo cambio en Ponentes

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -16,66 +16,47 @@ $( "#accordion" ).accordion({
 </script>
 
 
-<h1>Ponentes</h1><br>
-
-<div style="width: 100%; float:left;">
+<!--<h1>Ponentes</h1><br>-->
 <span>Del 22 al 26 de mayo en la Biblioteca Vasconcelos, en la Ciudad de México tendremos
 conversatorios sobre Turismo Sostenible con:</span><br><br>
 
 
+<div style="width: 50%; float:left;">
+
 <span class="tit_materiales">Moderadores</span><br>
 
-Alejandro García / <a href="http://oncetv-ipn.net/fciencia/" target="_blank">Reportero de Ciencia Canal Once</a><br>
-<a href="http://biografia.cine.hispavista.com/n90320-erik-estrada" target="_blank">Erick Estrada</a> / Actor y productor<br>
-<a href="https://twitter.com/ViajaBonito?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2Fwww.viajabonitomx.com%2F" target="_blank">Marco Daniel Guzmán</a> / <a href="http://www.viajabonitomx.com/
+- Alejandro García / <a href="http://oncetv-ipn.net/fciencia/" target="_blank">Reportero de Ciencia Canal Once</a><br>
+- <a href="http://biografia.cine.hispavista.com/n90320-erik-estrada" target="_blank">Erick Estrada</a> / Actor y productor<br>
+- <a href="https://twitter.com/ViajaBonito?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2Fwww.viajabonitomx.com%2F" target="_blank">Marco Daniel Guzmán</a> / <a href="http://www.viajabonitomx.com/
 " target="_blank">Viaja Bonito</a><br><br>
 
 
 <span class="tit_materiales">Ponentes</span><br>
 
-Lucía Ruiz Bustos / <a href="https://adventuremexico.travel/organizaciones/conanp/" target="_blank">Comisión Nacional de Áreas Protegidas (<span class="conabio">Conanp</span>)</a><br>
-Claudia Valeria Sánchez / <a href="http://www.rita.com.mx/" target="_blank">Red Indígena de Turismo de México</a><br>
-Julia Carabias Lilo / <a href="http://naturamexicana.org.mx/" target="_blank">Natura Mexicana</a><br>
-<a href="https://www.facebook.com/alonsopatadeperrovera/?pnref=story" target="_blank">Alonso Vera Cantú</a> / Pata de Perro<br>
-<a href="http://ib.unam.mx/directorio/194" target="_blank">Luis Zambrano</a> / Instituto de Biología UNAM<br>
-<a href="http://www.sedema.cdmx.gob.mx/secretaria/estructura/22" target="_blank">Luis Fueyo</a> / Comisión de Recursos Naturales de la Ciudad de México (<span class="conabio">Corena</span>)<br>
-Rodolfo Salinas / <a href="http://natoure.org/" target="_blank">Natoure.org</a><br>
-<a href="http://www.radioformula.com.mx/perfil.asp" target="_blank">Arturo Hernández</a> / <a href="https://www.facebook.com/Supercivicosmx/?fref=ts
-" target="_blank">Supercívicos</a><br>
-Renata Cao De la Fuente / <a href="http://www.biodiversidad.gob.mx/corredor/corredor.html" target="_blank"><span class="conabio">Conabio</span></a><br>
-<a href="https://www.gob.mx/sectur/estructuras/mtra-maria-teresa-solis-trejo" target="_blank">Teresa Solís</a> / <span class="conabio">Sectur</span><br>
-Vicente Ferreyra / <a href="http://sustentur.com.mx/" target="_blank"><span class="conabio">Sustentur</span></a><br>
-<br><br>
-
-
+- Lucía Ruiz Bustos / <a href="https://adventuremexico.travel/organizaciones/conanp/" target="_blank">Comisión Nacional de Áreas Protegidas (<span class="conabio">Conanp</span>)</a><br>
+- Claudia Valeria Sánchez / <a href="http://www.rita.com.mx/" target="_blank">Red Indígena de Turismo de México</a><br>
+- Julia Carabias Lilo / <a href="http://naturamexicana.org.mx/" target="_blank">Natura Mexicana</a><br>
+- <a href="https://www.facebook.com/alonsopatadeperrovera/?pnref=story" target="_blank">Alonso Vera Cantú</a> / Pata de Perro<br>
+- <a href="http://ib.unam.mx/directorio/194" target="_blank">Luis Zambrano</a> / Instituto de Biología UNAM<br>
+- <a href="http://www.sedema.cdmx.gob.mx/secretaria/estructura/22" target="_blank">Luis Fueyo</a> / Comisión de Recursos Naturales de la Ciudad de México (<span class="conabio">Corena</span>)<br>
+- Rodolfo Salinas / <a href="http://natoure.org/" target="_blank">Natoure.org</a><br>
+- <a href="http://www.radioformula.com.mx/perfil.asp" target="_blank">Arturo Hernández</a> / <a href="https://www.facebook.com/Supercivicosmx/?fref=ts" target="_blank">Supercívicos</a><br>
+- Renata Cao De la Fuente / <a href="http://www.biodiversidad.gob.mx/corredor/corredor.html" target="_blank"><span class="conabio">Conabio</span></a><br>
+- <a href="https://www.gob.mx/sectur/estructuras/mtra-maria-teresa-solis-trejo" target="_blank">Teresa Solís</a> / <span class="conabio">Sectur</span><br>
+- Vicente Ferreyra / <a href="http://sustentur.com.mx/" target="_blank"><span class="conabio">Sustentur</span></a><br>
+<br>
+<br>
 
 </div>
 
 
-<div style="width: 50%; display: none; float:left;">
+<div style="width: 50%; display: ; text-align: center; float:left;">
 
-<a href="http://www.puma.unam.mx/diplomado/index.php/cuerpo-docente/1-cuerpo-docente/20-pedro-alvarez-icaza" target="_blank">Mtro. Pedro Álvarez Icaza</a><br>
-<a href="http://www.ceiba.org.mx/quienes-somos/miembros/name/alfredo-arellano-guillermo/" target="_blank">Biól. Alfredo Arellano Guillermo</a><br>
-<a href="http://www.conafor.gob.mx:8080/documentos/docs/7/2355CV%20Enrique%20Jardel.pdf" target="_blank">M. en C. Enrique Jardel Peláez</a><br>
-<a href="https://www.youtube.com/watch?v=v8vxiwA4Ec4" target="_blank">Jordi Arenas</a><br>
-<a href="https://www.gob.mx/conanp/estructuras/andrew-john-rhodes-espinoza" target="_blank">Biól. Andrew Rhodes</a><br>
-<a href="http://causanatura.org/acerca-de-causanatura/ernesto-herrera/" target="_blank">Dr. Ernesto Herrera Guerra</a><br>
-<a href=" https://mx.linkedin.com/in/ra%C3%BAl-cort%C3%A9s-fern%C3%A1ndez-581a1a3b" target="_blank">Lic. Raúl Cortés</a><br><br>
-
-
-
-<a href="http://conabio.inaturalist.org/people/heliographyst" target="_blank">Biól. Roberto Arreola Alemón</a><br>
-<a href="http://conabio.inaturalist.org/people/elizatorres" target="_blank">Biól. Elizabeth Torres Bahena</a><br>
-<a href="https://www.linkedin.com/in/astrid-domínguez-guerrero-a9833498" target="_blank">D.G. Astrid Domínguez Guerrero</a><br><br>
-
-
-<a href="http://www.mexicopelagico.org/#creditos" target="_blank">Jerónimo Prieto</a><br>
-<a href="https://en.wikipedia.org/wiki/Jorge_Cervera_Hauser" target="_blank">Jorge Cervera Hauser<b</a><br>
-<a href="http://rcastrocr.wix.com/lacuchara#!bio" target="_blank">Ricardo Castro</a><br><br>
-
-
-<span class="tit_materiales">Grupo musical invitado</span><br>
-<a href="https://www.facebook.com/fechascasaverdecolectivo" target="_blank">Casa Verde Colectivo</a>
+<p>
+  <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/calendario_7SDB.jpg" target="_blank"><img src="http://www.biodiversidad.gob.mx/Difusion/news/images/calendario_7SDB.jpg" width="200" height="260" alt=""/></a><br>
+  <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/calendario_7SDB.jpg" target="_blank">Consulta el programa de la<br>
+7a. Semana de la Diversidad Biológica</a>
+</p>
 </div>
 
 
