e9883796a92b0ef63537c43bc80ae57ab33d3d86
jaluifa <lfabiola@gmail.com>
2018-05-15 19:59:15 -0500

    Actuallizacion de ponentes

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -24,42 +24,45 @@ conversatorios sobre Historias de éxito: acciones por la naturaleza con:</span>
 <div style="width: 50%; float:left;">
 
 <span class="tit_materiales">Moderadores</span><br>
-
-- Karla Iberia Sánchez<br>
-- Miguel Bárcena<br>
-- Iván Carrillo<br>
-- Iñaki Manero
+<ul>
+  <li><a href="https://es.wikipedia.org/wiki/Karla_Iberia_S%C3%A1nchez" target="_blank">Karla Iberia Sánchez</a></li>
+  <li><a href="https://www.facebook.com/miguelbarcenad" target="_blank">Miguel Bárcena</a></li>
+  <li><a href="http://carrillazo.com/" target="_blank">Iván Carrillo</a></li>
+  <li><a href="https://mixfm.mx/mix-nacional/mix-cdmx/locutores-cdmx/inaki-manero/" target="_blank">Iñaki Manero</a></li>
+</ul>
 <br><br>
 
 
 <span class="tit_materiales">Ponentes</span><br>
-
-
-- Adriana Lebrija / <span class="conabio">Conabio</span><br>
-- Aketzalli González<br>
-- Alexander Hotmann<br>
-- Alicia Mastretta Yanes / <span class="conabio">Conabio</span><br>
-- Ana Cruz<br>
-- Carlos Galindo Leal / <span class="conabio">Conabio</span><br>
-- Claudia Lewy / directora de zoológicos de la CDMX<br>
-- Cristina Martínez Garza / Centro de Investigación en Biodiversidad y Conservación (CIByC)<br>
-- Eliane Cecon / Centro Regional de Investigaciones Multidisciplinarias (CRIM)<br>
-- Elizabeth Torres Bahena / <span class="conabio">Conabio</span><br>
-- Emmanuel Rivera / <span class="conabio">Conabio</span><br>
-- Hesiquio Benítez Díaz / <span class="conabio">Conabio</span><br>
-- Idalia Rojas
-- Ignacio March Mifsut / <span class="conabio">Conanp</span><br>
-- José Bernal Stoopen<br>
-- María José Espinosa Rivero / COBI<br>
-- Moisés Méndez Toribio / Centro Regional de Investigaciones Multidisciplinarias (CRIM)<br>
-- Pedro Álvarez Icaza Longoria / <span class="conabio">Conabio</span><br>
-- Paola Mosig / <span class="conabio">Conabio</span><br>
-- Rainer Ressl / <span class="conabio">Conabio</span><br>
-- Roberto Arreola Alemón / <span class="conabio">Conabio</span><br>
-- Roberto Cerda / Restore Coral<br>
-- Wolke Tobon / <span class="conabio">Conabio</span><br>
+<ul>
+<li><a href="http://transportarte.com.mx/evento/adriana-lebrija/" target="_blank">Adriana Lebrija</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.linkedin.com/in/aketzalli-gonz%C3%A1lez-52a846149/" target="_blank">Aketzalli González</a></li>
+<li><a href="https://es.wikipedia.org/wiki/Alexander_Holtmann" target="_blank">Alexander Holtmann</a></li>
+<li><a href="https://github.com/AliciaMstt" target="_blank">Alicia Mastretta Yanes</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://moreliafilmfest.com/jurado/ana-cruz/" target="_blank">Ana Cruz</a></li>
+<li><a href="https://www.gob.mx/conabio/estructuras/dr-carlos-enrique-galindo-leal" target="_blank">Carlos Galindo Leal</a> / <span class="conabio">Conabio</span></li>
+<li><a href="http://www.sedema.cdmx.gob.mx/secretaria/estructura/18" target="_blank">Claudia Lewy</a> / Directora de zoológicos de la CDMX</li>
+<li><a href="http://www.mbibyc.mx/index.php/tutores/14-contenido/83-dra-martinez" target="_blank">Cristina Martínez Garza</a></span> / Centro de Investigación en Biodiversidad y Conservación (CIByC)</li>
+<li><a href="https://www.crim.unam.mx/web/node/843" target="_blank">Eliane Ceccon</a> / Centro Regional de Investigaciones Multidisciplinarias (CRIM)</li>
+<li><a href="http://www.naturalista.mx/people/elizatorres" target="_blank">Elizabeth Torres Bahena</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.linkedin.com/in/fabiola-l%C3%B3pez-salda%C3%B1a-0783462a/" target="_blank">Fabiola López Saldaña</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.gob.mx/conabio/estructuras/biol-hesiquio-benitez-diaz" target="_blank">Hesiquio Benítez Díaz</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.linkedin.com/in/idalia-claudia-rojas-barrera-39232128/" target="_blank">Idalia Rojas</a></li>
+<li><a href="https://www.linkedin.com/in/ignacio-march-7ab7a0111/" target="_blank">Ignacio March Mifsut</a> / Conanp</li>
+<li><a href="https://www.linkedin.com/in/jos%C3%A9-francisco-bernal-stoopen-a775057b/" target="_blank">José Bernal Stoopen</a></li>
+<li><a href="https://es.wikipedia.org/wiki/Jos%C3%A9_Sarukh%C3%A1n_Kermez" target="_blank">José Sarukhán</a> / <span class="conabio">Conabio</span></li>
+<li><a href="http://cobi.org.mx/" target="_blank">María José Espinosa Rivero</a> / COBI</li>
+<li><a href="http://www.inecol.edu.mx/personal/index.php/ambiente-y-sustentabilidad/21-miguel-e-equihua-zamora" target="_blank">Miguel Equihua Zamora</a> / INECOL</li>
+<li><a href="https://www.linkedin.com/in/mois%C3%A9s-m%C3%A9ndez-toribio-08436551/" target="_blank">Moisés Méndez Toribio</a> / Centro Regional de Investigaciones Multidisciplinarias (CRIM)</li>
+<li><a href="https://www.gob.mx/conabio/estructuras/mtro-pedro-carlos-alvarez-icaza-longoria" target="_blank">Pedro Álvarez Icaza Longoria</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.gob.mx/conabio/estructuras/dr-rainer-andreas-ressl" target="_blank">Rainer Ressl</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.linkedin.com/in/roberto-arreola-alem%C3%B3n-a25631146/" target="_blank">Roberto Arreola Alemón</a> / <span class="conabio">Conabio</span></li>
+<li><a href="https://www.linkedin.com/in/roberto-cerda-0765115b/" target="_blank">Roberto Cerda</a> / Restore Coral</li>
+<li><a href="https://www.researchgate.net/profile/Wolke_Tobon" target="_blank">Wolke Tobon</a> / <span class="conabio">Conabio</span></li>
+</ul>
+<br>
 	
-<br><br>
+<br>
 
 </div>
 
