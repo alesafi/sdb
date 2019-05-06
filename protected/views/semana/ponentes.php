720a3d402441958bee218290bb7d3461f76f2fc6
jaluifa <lfabiola@gmail.com>
2019-05-06 15:51:33 -0500

    Cambio de item, programa e imagotipo Bella epoca

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -17,63 +17,15 @@ $( "#accordion" ).accordion({
 
 
 <!--<h1>Ponentes</h1><br>-->
-<span>Del 21 al 25 de mayo tendremos
-conversatorios sobre Nuestra biodiversidad, nuestra alimentación, nuestra salud.<br>
-<br>
-Próximamente los publicaremos</span><br><br>
+<span>Del 21 al 25 de mayo tendremos actividades sobre Nuestra biodiversidad, nuestra alimentación, nuestra salud. <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_9SDB_carta.jpg" target="_blank">Consulta el programa de la 9a. Semana de la Diversidad Biológica</a>
+</span><br><br>
 
 
-<div style="width: 50%; float:left; display: none;">
-
-<span class="tit_materiales">Moderadores</span><br>
-<ul>
-  <li><a href="https://es.wikipedia.org/wiki/Karla_Iberia_S%C3%A1nchez" target="_blank">Karla Iberia Sánchez</a></li>
-  <li><a href="https://www.facebook.com/miguelbarcenad" target="_blank">Miguel Bárcena</a></li>
-  <li><a href="http://carrillazo.com/" target="_blank">Iván Carrillo</a></li>
-  <li>Leonardo Ferreira</li>
-</ul>
-<br><br>
-
-
-<span class="tit_materiales">Ponentes</span><br>
-<ul>
-<li><a href="http://transportarte.com.mx/evento/adriana-lebrija/" target="_blank">Adriana Lebrija</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.linkedin.com/in/aketzalli-gonz%C3%A1lez-52a846149/" target="_blank">Aketzalli González</a></li>
-<li><a href="https://es.wikipedia.org/wiki/Alexander_Holtmann" target="_blank">Alexander Holtmann</a></li>
-<li><a href="https://github.com/AliciaMstt" target="_blank">Alicia Mastretta Yanes</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://moreliafilmfest.com/jurado/ana-cruz/" target="_blank">Ana Cruz</a></li>
-<li><a href="https://www.gob.mx/conabio/estructuras/dr-carlos-enrique-galindo-leal" target="_blank">Carlos Galindo Leal</a> / <span class="conabio">Conabio</span></li>
-<li><a href="http://www.sedema.cdmx.gob.mx/secretaria/estructura/18" target="_blank">Claudia Lewy</a> / Directora de zoológicos de la CDMX</li>
-<li><a href="http://www.mbibyc.mx/index.php/tutores/14-contenido/83-dra-martinez" target="_blank">Cristina Martínez Garza</a></span> / Centro de Investigación en Biodiversidad y Conservación (CIByC)</li>
-<li><a href="https://www.crim.unam.mx/web/node/843" target="_blank">Eliane Ceccon</a> / Centro Regional de Investigaciones Multidisciplinarias (CRIM)</li>
-<li><a href="http://www.naturalista.mx/people/elizatorres" target="_blank">Elizabeth Torres Bahena</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.linkedin.com/in/fabiola-l%C3%B3pez-salda%C3%B1a-0783462a/" target="_blank">Fabiola López Saldaña</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.gob.mx/conabio/estructuras/biol-hesiquio-benitez-diaz" target="_blank">Hesiquio Benítez Díaz</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.linkedin.com/in/idalia-claudia-rojas-barrera-39232128/" target="_blank">Idalia Rojas</a></li>
-<li><a href="https://www.linkedin.com/in/ignacio-march-7ab7a0111/" target="_blank">Ignacio March Mifsut</a> / Conanp</li>
-<li><a href="https://www.linkedin.com/in/jos%C3%A9-francisco-bernal-stoopen-a775057b/" target="_blank">José Bernal Stoopen</a></li>
-<li><a href="https://es.wikipedia.org/wiki/Jos%C3%A9_Sarukh%C3%A1n_Kermez" target="_blank">José Sarukhán</a> / <span class="conabio">Conabio</span></li>
-<li><a href="http://cobi.org.mx/" target="_blank">María José Espinosa Rivero</a> / COBI</li>
-<li><a href="http://www.inecol.edu.mx/personal/index.php/ambiente-y-sustentabilidad/21-miguel-e-equihua-zamora" target="_blank">Miguel Equihua Zamora</a> / INECOL</li>
-<li><a href="https://www.linkedin.com/in/mois%C3%A9s-m%C3%A9ndez-toribio-08436551/" target="_blank">Moisés Méndez Toribio</a> / Centro Regional de Investigaciones Multidisciplinarias (CRIM)</li>
-<li><a href="https://www.gob.mx/conabio/estructuras/mtro-pedro-carlos-alvarez-icaza-longoria" target="_blank">Pedro Álvarez Icaza Longoria</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.linkedin.com/in/roberto-arreola-alem%C3%B3n-a25631146/" target="_blank">Roberto Arreola Alemón</a> / <span class="conabio">Conabio</span></li>
-<li><a href="https://www.linkedin.com/in/roberto-cerda-0765115b/" target="_blank">Roberto Cerda</a> / Restore Coral</li>
-<li><a href="https://www.researchgate.net/profile/Wolke_Tobon" target="_blank">Wolke Tobon</a> / <span class="conabio">Conabio</span></li>
-</ul>
-<br>
-	
-<br>
-
-</div>
-
-
-<div style="width: 50%; display: ; text-align: center; float:left; display:none;">
+<div style="width: 100%; display: ; text-align: center; float:left; display:;">
 
 <p>
-  <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_8SDB_carta.jpg" target="_blank"><img src="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_8SDB_carta.jpg" width="200" height="155" alt=""/></a><br>
-  <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_8SDB_carta.jpg" target="_blank">Consulta el programa de la<br>
-9a. Semana de la Diversidad Biológica</a>
+  <a href="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_9SDB_carta.jpg" target="_blank"><img src="http://www.biodiversidad.gob.mx/Difusion/news/images/programa_9SDB_carta.jpg" width="100%" alt=""/></a><br>
+  
 </p>
 </div>
 
