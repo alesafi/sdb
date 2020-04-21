60a917d8b96480ae61cd08356b0d20ab5d825966
jaluifa <lfabiola@gmail.com>
2020-04-21 17:48:21 -0500

    Cambios para Mayo: mes de la naturaleza mexicana

--- protected/views/semana/inicio.php
+++ protected/views/semana/inicio.php
@@ -1,4 +1,19 @@
-<p>La Comisi&oacute;n Nacional para el Conocimiento y Uso de la  Biodiversidad <span class="conabio">(Conabio)</span> te invita a participar activamente en la&nbsp;<strong>9a. Semana de la Diversidad Biol&oacute;gica</strong>, del <strong>21 al 25 de mayo </strong>del a&ntilde;o en curso,  realizando <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/admin">actividades de divulgaci&oacute;n</a><strong>.</strong></p>
+<p>Este mes de mayo termina la <strong>Década Internacional de la Diversidad Biológica</strong>, una década dedicada a hacer esfuerzos para promover el conocimiento, valoración y buen manejo de la naturaleza. La mayoría de los países del mundo celebraron el <strong>22 de mayo, día internacional de la diversidad biológica</strong>.</p>
+
+<p>En México, hemos celebrado nueve ediciones de la <strong>Semana de la Diversidad Biológica</strong> con una gran participación de la sociedad mexicana: escuelas, organizaciones de la sociedad civil, universidades, municipios, ejidos, comunidades, todos han participado promoviendo la naturaleza a través de charlas, conferencias, videos, salidas a playas, parques, arroyos, conciertos, manualidades, etc.</p>
+
+<p>Para las Naciones Unidas, el 2020 es el <strong>Año Internacional de la Salud de las Plantas</strong>, un tema realmente tan importante como amplio, que va desde la conservación de los ecosistemas hasta nuestra alimentación. El Convenio de Diversidad Biológica celebra el Día Internacional de la Diversidad Biológica en el 2020 con el tema <strong>“Nuestras soluciones están en la naturaleza”</strong>.</p>
+
+<p>Para participar en esta celebración, el Convenio de Diversidad Biológica sugiere los siguientes temas:</p>
+<ul>
+	<li>El entendimiento de la biodiversidad, su pérdida y soluciones para un futuro sostenible: la ciencia y el conocimiento tradicional</li>
+	<li>Distintas maneras de conservar la biodiversidad</li>
+	<li>Salud, agricultura, alimentación, seguridad alimentaria y biodiversidad</li>
+	<li>Gente, cultura y biodiversidad, cambio climático, biodiversidad y tierra</li>
+	<li>Nuestras soluciones están en la naturaleza</li>
+</ul>
+
+<p>Durante <strong>Mayo: mes de la naturaleza mexicana</strong>, como en los años anteriores, la Comisi&oacute;n Nacional para el Conocimiento y Uso de la  Biodiversidad <span class="conabio">(Conabio)</span> te invita a participar utilizando los matriales que hemos seleccionado y subiendo <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/admin">actividades en línea al calendario nacional de la página</a>.</p>
 
 <center>
 <h4><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=usuarios/create">¡Súmate y participa desde tu estado!</a> | 
@@ -6,26 +21,20 @@
 <br>
 </center>
 
-<div style="display: inline-block; width: 35%; float: left" id="programa_sdb">
+<!--<div style="display: inline-block; width: 35%; float: left" id="programa_sdb">
 <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" width="200" height="155" alt=""/></a<br><br>
 <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" target="_blank">Programa en la Librería<br>
 Rosario Castellanos</a><br><br>
-</div>
+</div>-->
   
-<div style="display: inline-block; width: 65%; float: left" id="video_sdb">
-<!--<iframe width="90%" height="315" src="https://www.youtube.com/embed/tg55eKqZ_WQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe><br><br>-->
+<!--<div style="display: inline-block; width: 65%; float: left" id="video_sdb">
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_video.png" width="25" height="22" /> <span class="tit_materiales">Lista de reproducción</span>
-<!-- Linea --><hr/>
-
+<hr/>
 <iframe width="100%" height="260" src="https://www.youtube.com/embed/tg55eKqZ_WQ?list=PLLAcoRHqCmNQqFpmvI0-jvk-8xF7WaXoZ" frameborder="0" allowfullscreen></iframe>	
-	
 </div>
-<br>
-<p>La década de la diversidad biológica de 2011 a 2020 tiene el propósito de aumentar las actividades de difusión para que todos conozcamos la situación de la diversidad biológica y participemos en su manejo sustentable, restauración y conservación.</p>
+<br>-->
 
-<p>Cada 22 de mayo se celebra el día de la diversidad biológica, este 2019 celebraremos con el tema <span class="slogan">"Nuestra biodiversidad, nuestra alimentación, nuestra salud"</span>.</p>
-
-<p>Organiza alguna conferencia o charla para el público general sobre biodiversidad, alimentación y salud o un evento que ayude al público a reflexionar sobre cómo la biodiversidad repercute en nuestra alimentación y salud. Si tienes alguna duda o requieres m&aacute;s informaci&oacute;n escr&iacute;benos a:&nbsp;<a href="mailto:sdb@conabio.gob.mx">sdb@conabio.gob.mx</a>.
+<p>Si tienes alguna duda o requieres m&aacute;s informaci&oacute;n escr&iacute;benos a:&nbsp;<a href="mailto:eventos@conabio.gob.mx">eventos@conabio.gob.mx</a>.
 </p>
 
 
@@ -34,9 +43,7 @@ Rosario Castellanos</a><br><br>
 
 <a href="https://www.cbd.int/2011-2020/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_2011_2020.png" width="147" height="59"/></a>&nbsp;&nbsp;
 	
-<a href="https://fondodeculturaeconomica.com/RedLibrerias.aspx?id_lib=4" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_libreriarc.png" width="229" height="128"/></a>
-	
-<a href="https://www.cbd.int/idb/2019/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_DIDB.jpg" width="150" height="84"/></a>
+<a href="https://www.cbd.int/idb/2020/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_DIDB.jpg" width="216" height="84"/></a>
 
 </center>
                         
