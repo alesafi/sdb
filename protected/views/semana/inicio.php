e9857fee1562e0c9d2ddad1a1a76203454695c4c
Alejandra Salazar <asalazar@conabio.gob.mx>
2021-04-22 10:47:23 -0500

    actualizacion de mes de naturaleza 2021

--- protected/views/semana/inicio.php
+++ protected/views/semana/inicio.php
@@ -1,37 +1,59 @@
-<p>La Comisi&oacute;n Nacional para el Conocimiento y Uso de la  Biodiversidad <span class="conabio">(Conabio)</span> te invita a participar activamente en la&nbsp;<strong>9a. Semana de la Diversidad Biol&oacute;gica</strong>, del <strong>21 al 25 de mayo </strong>del a&ntilde;o en curso,  realizando <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/admin">actividades de divulgaci&oacute;n</a><strong>.</strong></p>
+<p>El tema de la celebración del <strong><a href="https://www.cbd.int/biodiversity-day" target="_blank">Día Internacional de la Diversidad Biológica</a></strong> es <strong>“Somos parte de la solución”</strong>. El tema fue propuesto por el Convenio de Diversidad Biológica y consideramos que es de gran relevancia, y es precisamente en lo que la CONABIO ha encaminado múltiples esfuerzos por más de una década: incluir la participación de la sociedad para resolver los problemas ambientales. Por esto, desde el año pasado dejamos la Semana de la Diversidad Biológica y propusimos que celebraríamos todo un mes “Mayo, mes de la naturaleza mexicana”</p>
+
+<p>Además, en 2021 hay por lo menos tres eventos internacionales importantes:</p>
+
+<ul>
+	<li>Se inicia la <a href="https://www.decadeonrestoration.org/es" target="_blank">Década de las Naciones Unidas de la Restauración de los Ecosistemas</a> el 5 de junio.</li>
+	<li>Se inicia la <a href="https://www.oceandecade.org/" target="_blank">Década de las Naciones Unidas de las Ciencias Oceánicas para el Desarrollo Sostenible</a>.</li>
+	<li>Se adopta el <strong>Marco Global de Biodiversidad Post 2020</strong> en la <a href="https://www.cbd.int/meetings/COP-15" target="_blank">15º reunión de la Conferencia de las Partes</a> en Kunming, China del 11 al 24 de octubre.</li>
+</ul>
+
+<p>La década pasada celebramos con una gran participación de la sociedad mexicana: escuelas, organizaciones de la sociedad civil, universidades, municipios, ejidos, comunidades, se sumaron a colaborar,  promoviendo el conocimiento de la riqueza natural de México a través de charlas, conferencias, videos, salidas a playas, parques, arroyos, conciertos, manualidades, etc.
+
+<p>Para participar en esta celebración, el Convenio de Diversidad Biológica sugiere los siguientes temas:</p>
+<ul>
+<li>¿Qué puedo hacer para reducir mi <a href="https://www.biodiversidad.gob.mx/biodiversidad/que_hacer" target="_blank">huella ecológica?</a></li>
+<li>Consume responsablemente</li>
+	<ul>
+	<li>Conoce y consume de prácticas sostenibles de <a href="https://www.biodiversidad.gob.mx/diversidad/alimentos/consumidor-responsable" target="_blank">agricultura, ganadería y pesca</a></li>
+	<li>Conoce la situación de <a href="https://www.biodiversidad.gob.mx/diversidad/alimentos/peces/" target="_blank">peces y mariscos</a> y consume las opciones sostenibles</li>
+	</ul>
+<li>Promueve el <a href="https://www.biodiversidad.gob.mx/diversidad/sistemas-productivos/turismo" target="_blank">turismo sustentable</a></li> 
+<li>Conoce las especies de tu <a href="https://enciclovida.mx/explora-por-region?utf8=%E2%9C%93&pagina=1#5/25.026/-83.496" target="_blank">municipio y de las áreas protegidas</a></li>
+<li>Ayuda a los <a href="http://www.paismaravillas.mx/polinizadores/" target="_blank">polinizadores</a></li>
+<li>Participa en <a href="https://www.biodiversidad.gob.mx/cienciaciudadana/" target="_blank">ciencia ciudadana</a></li>
+</ul>
+
+</p>
+<p>Durante <strong>Mayo: mes de la naturaleza mexicana</strong>, como en los años anteriores, la Comisi&oacute;n Nacional para el Conocimiento y Uso de la  Biodiversidad <span class="conabio">(Conabio)</span> te invita a participar utilizando los matriales que hemos seleccionado y subiendo <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/admin">actividades en línea al calendario nacional de la página</a>.</p>
 
 <center>
 <h4><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=usuarios/create">¡Súmate y participa desde tu estado!</a> | 
-<a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/create">Publica y comparte tus actividades</a></h4>
+<a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php?r=semana/create">Publica y comparte tus actividades virtuales</a></h4>
 <br>
 </center>
 
-<div style="display: inline-block; width: 35%; float: left" id="programa_sdb">
+<!--<div style="display: inline-block; width: 35%; float: left" id="programa_sdb">
 <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" width="200" height="155" alt=""/></a<br><br>
 <a href="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/programa_9SDB_carta.jpg" target="_blank">Programa en la Librería<br>
 Rosario Castellanos</a><br><br>
-</div>
+</div>-->
   
-<div style="display: inline-block; width: 65%; float: left" id="video_sdb">
-<iframe width="90%" height="315" src="https://www.youtube.com/embed/tg55eKqZ_WQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe><br><br>
+<!--<div style="display: inline-block; width: 65%; float: left" id="video_sdb">
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_video.png" width="25" height="22" /> <span class="tit_materiales">Lista de reproducción</span>
+<hr/>
+<iframe width="100%" height="260" src="https://www.youtube.com/embed/tg55eKqZ_WQ?list=PLLAcoRHqCmNQqFpmvI0-jvk-8xF7WaXoZ" frameborder="0" allowfullscreen></iframe>	
 </div>
+<br>-->
 
-<p>La década de la diversidad biológica de 2011 a 2020 tiene el propósito de aumentar las actividades de difusión para que todos conozcamos la situación de la diversidad biológica y participemos en su manejo sustentable, restauración y conservación.</p>
-
-<p>Cada 22 de mayo se celebra el día de la diversidad biológica, este 2019 celebraremos con el tema <span class="slogan">"Nuestra biodiversidad, nuestra alimentación, nuestra salud"</span>.</p>
-
-<p>Organiza alguna conferencia o charla para el público general sobre biodiversidad, alimentación y salud o un evento que ayude al público a reflexionar sobre cómo la biodiversidad repercute en nuestra alimentación y salud. Si tienes alguna duda o requieres m&aacute;s informaci&oacute;n escr&iacute;benos a:&nbsp;<a href="mailto:sdb@conabio.gob.mx">sdb@conabio.gob.mx</a>.
+<p>Si tienes alguna duda o requieres m&aacute;s informaci&oacute;n escr&iacute;benos a:&nbsp;<a href="mailto:eventos@conabio.gob.mx">eventos@conabio.gob.mx</a>.
 </p>
 
 
 <center>
-<a href="http://www.conabio.gob.mx/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_conabio.png" width="178" height="59"/></a>&nbsp;&nbsp;
-
-<a href="https://www.cbd.int/2011-2020/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_2011_2020.png" width="147" height="59"/></a>&nbsp;&nbsp;
+<a href="https://www.gob.mx/conabio" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_conabio.png" width="178" height="59"/></a>&nbsp;&nbsp;
 	
-<a href="https://fondodeculturaeconomica.com/RedLibrerias.aspx?id_lib=4" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_libreriarc.png" width="229" height="128"/></a>
-	
-<a href="https://www.cbd.int/idb/2019/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_DIDB.jpg" width="150" height="84"/></a>
+<a href="https://www.cbd.int/idb/2020/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/logo_DIDB.png" width="216" height="84"/></a>
 
 </center>
                         
--- protected/views/semana/mat_ninos.php
+++ protected/views/semana/mat_ninos.php
@@ -1,30 +1,34 @@
 <h1>Materiales para niños</h1>
 
-  
+  <div id="aagro">
 	<!--<h4>Agrobiodiversidad</h4>--><br>
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_web.png" width="25" height="22" /> <span class="tit_materiales">Web</span>
 	<!-- Linea --><hr/>
 	<ul>
-		<li><a href="http://www.paismaravillas.mx/explora.html#campo" target="_blank">Ecosistemas de México (campo)</a></li>
-		<li><a href="http://www.paismaravillas.mx/explora.html#pacifico" target="_blank">Pacífico</a></li>
-		<li><a href="http://www.paismaravillas.mx/explora.html#golfo" target="_blank">Golfo y Caribe</a></li> 
-	    <li><a href="http://www.paismaravillas.mx/consumo_videos.html" target="_blank">País de las Maravillas</a></li>
-	    <li><a href="http://www.paismaravillas.mx/explora.html#campo">Campo</a></li>
+		<li><a href="http://www.paismaravillas.mx" target="_blank">México, país de las maravillas</a></li>
+		<li><a href="http://www.paismaravillas.mx/megadiverso.html" target="_blank">México megadiverso</a></li>
+		<li><a href="http://www.paismaravillas.mx/recortables.html" target="_blank">Recortables</a></li> 
+	    <li><a href="http://www.paismaravillas.mx/vecinos.html" target="_blank">Vecinos verdes</a></li>
+		<li><a href="http://www.paismaravillas.mx/vecinos.html" target="_blank">Especies invasoras - ¿qué puedo hacer?</a></li>
+		<li><a href="http://www.paismaravillas.mx/quedana.html" target="_blank">¿Cómo dañamos los ecosistemas?</a></li>
+		<li><a href="http://www.paismaravillas.mx/quepasa.html" target="_blank">¿Qué pasa cuando eliminamos?</a></li>
+		<li><a href="http://www.paismaravillas.mx/entremas.html" target="_blank">Entre más somos</a></li>
+		<li><a href="http://www.paismaravillas.mx/conservar.html" target="_blank">¿Por qué hay que conservarlos?</a></li>
+		<li><a href="http://www.paismaravillas.mx/quehacer.html" target="_blank">¿Qué puedo hacer?</a></li>
+		<li><a href="http://www.paismaravillas.mx/cuidas.html" target="_blank">¿Cuidas o destruyes?</a></li>
+		<li><a href="http://www.paismaravillas.mx/consumo_videos.html" target="_blank">Consume responsablemente</a></li>
 	</ul>
 		
-		
+
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_video.png" width="25" height="22" /> <span class="tit_materiales">Videos</span>
-	<!-- Linea --><hr/>	
+	<hr/>	
 	<ul>
-		<li><a href="https://www.youtube.com/watch?v=1o2dj8TLFmA&list=PLLAcoRHqCmNSRYOrx-s0B_5t0Qc99SIoT&index=2" target="_blank">Agricultura</a></li>
-		<li><a href="https://www.youtube.com/watch?v=bKqOwG2DJlQ" target="_blank">Alimentos orgánicos y nutrición</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=cgFRwjwlrvE" target="_blank">Visita al huerto biointensivo</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=nOFmmgRm-UI" target="_blank">Huertos orgánicos</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=Wi0JNp1ZCaY&list=PLLAcoRHqCmNSRYOrx-s0B_5t0Qc99SIoT&index=5" target="_blank">Ganadería</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=7L8RBxOMWac" target="_blank">Ganadería sustentable</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=rlBD49J2U4Q&list=PLLAcoRHqCmNSRYOrx-s0B_5t0Qc99SIoT&index=6" target="_blank">Pesca</a></li>
-	    <li><a href="https://www.youtube.com/watch?v=eKfjTKlOojE" target="_blank">Acuicultura sustenable</a></li>
+		<li><a href="https://youtube.com/playlist?list=PLLAcoRHqCmNSR8FrRKO0S4G_rLTEJTTdG" target="_blank">Concierto "El llamado"</a></li>
+		<li><a href="https://www.youtube.com/watch?v=TOT5OSP1N3I" target="_blank">Los guardianes del medio ambiente</a></li>
 		<br>
 	</ul>
-
+</div>
 </section>
+
+<!-- Two -->
+<section id="two"></section>
